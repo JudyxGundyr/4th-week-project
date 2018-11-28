408a2da8349fa54194550bfc79cd18775c0e4969
David <ighammudi@gmail.com>
2018-11-28 11:52:34 +0100

    28th nov modifications

--- Battleship.py
+++ Battleship.py
@@ -20,10 +20,15 @@ def random_col(map):
 
 def creat_map():
     map = []
-    for i in range(0,5):
-        row  = ['0','0','0','0','0']
+    for i in range(0,6):
+        row  = ['O','O','O','O','O','O']
         map.append(row)
     return map
+
+def map_borders():
+    for y in range(len(map)):
+        border = ['----------------------']
+        print(border.join(map))
 
 print('Welcome to battleship','\033[94m')
 print('Please enter your name\'s')
@@ -49,44 +54,41 @@ print(random_player(players), 'starts the game')
 
 hit_count = 0
 
-for turn in range(4):
-   while True: 
-    try:
-        guess_row = int(input('guess row: (allowed values: 0-4)'))
-        guess_col = int(input('guess col: (allowed values: 0-4)'))
-    except ValueError:
-        print('OPPS....please Enter a valid number!')
-        continue     
-    
-    if (guess_row == ship_row_1 and guess_col == ship_col_1):
-        hit_count = hit_count + 1
-        map[guess_row][guess_col] ='*'
-    elif (guess_row == ship_row_2 and guess_col == ship_col_2):
-        hit_count = hit_count + 1
-        map[guess_row][guess_col] ="*"
-        print("Good job!!")
-        if hit_count == 1:
-            print('you sunk first battleship!')
-        elif hit_count == 2:
-            print('you sunk second battleship')
+for turn in range(11):
+    while True: 
+        try:
+            guess_row = int(input('guess row: (allowed values: 0-5)'))
+            guess_col = int(input('guess col: (allowed values: 0-5)'))
+        except ValueError:
+            print('OPPS....please Enter a valid number!')
+            continue     
+        
+        if (guess_row == ship_row_1 and guess_col == ship_col_1):
+            hit_count = hit_count + 1
+            map[guess_row][guess_col] ='*'
+        elif (guess_row == ship_row_2 and guess_col == ship_col_2):
+            hit_count = hit_count + 1
+            map[guess_row][guess_col] ="*"
+            print("Good job!!")
+        else:   
+            if (guess_row < 0 or guess_row > 5) and (guess_col < 0 or guess_col > 5):
+                    print("HOPPA, you sailed too far")
+            elif(map[guess_row][guess_col] == 'x'):
+                    print('Already been chossen')
+            if hit_count == 1:
+                print('you sunk first battleship!')
+            elif hit_count == 2:
+                print('you sunk second battleship')
+            else:
+                print ('you missed')
+                map[guess_row][guess_col] = 'x'
+            # except ValueError:
+            #     continue
+
+            print (turn + 1,'turn')   
         print_map(map)
-        break
-    else:   
-        if (guess_row < 0 or guess_row > 5) and (guess_col < 0 or guess_col > 5):
-                print("HOPPA, you sailed too far")
+    
 
-        elif(map[guess_row][guess_col] == 'x'):
-                print('Already been chossen')
-        else:
-            print ('you missed')
-            map[guess_row][guess_col] = 'x'
-        # except ValueError:
-        #     continue
-
-        print (turn + 1,'turn')   
-    print_map(map)
-
-   
 # print('1st ship is hidden:')
 
 # print(ship_row_1)
@@ -96,3 +98,4 @@ for turn in range(4):
 
 # print(ship_row_2)
 # print(ship_col_2)
+   
